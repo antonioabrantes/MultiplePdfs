a29be661c39df1f7de94e509ddd73a52814dbb74
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-11-27 09:53:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub 
+from langchain_huggingface import HuggingFaceEmbeddings
+
 
 import os
 from dotenv import load_dotenv
